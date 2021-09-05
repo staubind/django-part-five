db7e58a20dea11fe5c917254842e873a61b4f191
Dan St. Aubin <staubindj@gmail.com>
2021-09-05 12:15:10 -0500

    added in basic routes. all set up for registration to be done

---
+++ learning_users/basic_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from basic_app import views
+
+# TEMPLATE URLS! Need this other was template tag urls don't work.
+app_name = 'basic_app'
+
+urlpatterns = [
+    path('register', views.register, name='register'),
+
+]
